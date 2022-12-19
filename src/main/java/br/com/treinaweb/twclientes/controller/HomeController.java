4fb70840572c01bdb7b61db0b1abc13026225ed2
Íris <iris.balk04@gmail.com>
2022-12-19 15:12:19 -0300

    twclientes
    Entendendo melhor como funciona a camada de view, usando thymeleaf, e como passar informações para a camada de controller.

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -1,9 +1,12 @@
 package br.com.treinaweb.twclientes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -13,4 +16,36 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/mensagem")
+    public ModelAndView mensagem() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Mensagem vinda do servidor!");
+        
+        return modelAndView;
+    }
+
+    @GetMapping("/saudacao/{nome}")
+    public ModelAndView saudacao(@PathVariable String nome) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+        
+        return modelAndView;
+    }
+
+    // cumprimento?nome=Ana
+    @GetMapping("/cumprimento")
+    public ModelAndView cumprimento(@RequestParam(required = false, defaultValue = "Iris") String nome) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("cumprimento");
+        modelAndView.addObject("nome", nome);
+        
+        return modelAndView;
+    }
+
 }
